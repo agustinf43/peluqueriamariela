4a04a33a8015e53e0f4d4c03a94cb9f60f1c5def
agustin <agustinfernandez43tb@gmail.com>
2024-10-24 13:53:16 -0300

    barra de navegacion y titulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./Componentes/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
