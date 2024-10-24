e65ec35db69d4bb4e6719373263c8573d87dc700
agustin <agustinfernandez43tb@gmail.com>
2024-10-24 14:53:03 -0300

    agregue  Acerca de "principal"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./Componentes/navbar/navbar.component";
+import { PrincipalComponent } from "./Componentes/principal/principal.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, PrincipalComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
