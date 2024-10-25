cb46b22ecb9a311c23ab9bf40a22d1f3950c560a
agustin <agustinfernandez43tb@gmail.com>
2024-10-25 00:36:17 -0300

    servicios y galeria agregados

--- src/app/Componentes/servicios/servicios.component.ts
+++ src/app/Componentes/servicios/servicios.component.ts
@@ -1,12 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-servicios',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './servicios.component.html',
   styleUrl: './servicios.component.css'
 })
 export class ServiciosComponent {
+  
+  servicios=[
+    {
+      nombre:"Corte De Cabello",
+      imagen:"/assets/img/servicios/fotoCortes.jpg",
+      alt:"imagen de Servicio de Cortes de pelo",
+      descripcion:"Renueva tu estilo con nuestro corte de cabello profesional."
 
+    },{
+      nombre:"Pintura",
+      imagen:"/assets/img/servicios/fotoPintura.jpg",
+      alt:"imagen de Servicio de Pintura",
+      descripcion:"Transforma tu estilo con nuestro servicio de pintura."
+      
+    },{
+      nombre:"Peinados",
+      imagen:"/assets/img/servicios/fotoPeinado.jpg",
+      alt:"Imagen de Servicio de Peinados",
+      descripcion:"Prepárate para brillar con nuestros peinados para ocasiones especiales."
+    }
+
+  ]
+   
+  
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { initFlowbite } from 'flowbite';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./Componentes/navbar/navbar.component";
 import { PrincipalComponent } from "./Componentes/principal/principal.component";
+import { ServiciosComponent } from "./Componentes/servicios/servicios.component";
+import { CommonModule } from '@angular/common';
+import { GaleriaComponent } from "./Componentes/galeria/galeria.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, PrincipalComponent],
+  imports: [RouterOutlet, NavbarComponent, PrincipalComponent, ServiciosComponent, CommonModule, GaleriaComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
