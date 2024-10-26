2f66aa3341f47b46e0f90b864a219f353f85e54f
agustin <agustinfernandez43tb@gmail.com>
2024-10-25 21:02:12 -0300

    servicios agregados

--- src/app/Componentes/galeria/galeria.component.ts
+++ src/app/Componentes/galeria/galeria.component.ts
@@ -1,12 +1,44 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-galeria',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './galeria.component.html',
   styleUrl: './galeria.component.css'
 })
 export class GaleriaComponent {
+  /*
+  galeriaImagenes=[{
+    imgUrl:"https://comfenalcoquindio.com/wp-content/uploads/2022/05/tecnico-en-peluqueria.jpg"
+  },{
+    imgUrl:"https://www.altacapacitacion.com/product_images/cursos-en-acp/aprender/2__21703_std.jpg"
+  },{
+    imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdYzOnzh3Lb8pq8ejdJihSkL3OYFpwkIPIUw&s"
+  },{
+    imgUrl:"https://cursos.com/wp-content/uploads/2021/01/historia-de-la-peluqueria-710x570.jpg.webp"
+  },{
+    imgUrl:"https://www.academiacasanova.com/wp-content/uploads/2024/08/Academia-Casanova-peluqueria-y-barberia-masculina.jpg"
+  },{
+    imgUrl:"https://robertocarrillo1998.es/wp-content/uploads/2022/12/blog13.jpg.webp"
+  },{
+    imgUrl:"https://img.freepik.com/foto-gratis/mujer-recibiendo-tratamiento-peluqueria_23-2149230957.jpg"
+  },{
+    imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSYqb5fHJDh2J2e6Cj_CpBIbGH4cXj1k-lVxFR2LetcCf-btKrPendvtHOfPADAR_kLcf0&usqp=CAU"
+  },{
+    imgUrl:"https://forjandoelfuturo.com.ar/wp-content/uploads/2021/12/peluqueria.jpg"
+  },{
+    imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8PQfiRFUrjcQJvSnYmjIxjXvN3rsN5AZ1Dg&s"
+  },{
+    imgUrl:"https://www.eurostil.com/wp-content/uploads/2021/12/blog-novedades-1024x1024.jpg"
+  },{
+    imgUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNleoq40gIbYyMEqlnuHM-xWv1wtG-yEg_Xg&s"
+  }
+  
+
+  ]*/ //Para terminar
+
+
 
 }
