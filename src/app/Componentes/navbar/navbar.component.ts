78033f7941a0be62e810dd6883b37c8d9892814b
agustin <agustinfernandez43tb@gmail.com>
2025-02-23 22:27:18 -0300

    barra de navegación actualizada

--- src/app/Componentes/navbar/navbar.component.ts
+++ src/app/Componentes/navbar/navbar.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   
+
+  //para navegar en la pagina
   navegarAlComponente(ComponenteId: string): void {
     const element = document.getElementById(ComponenteId);
     if (element) {
@@ -22,9 +25,54 @@ export class NavbarComponent {
     alert("Proximamente")
   }
 
+  //ocultar barra de navegacion
+
+  isFixed = false; // Controla si la barra está fija
+  isVisible = true; // Controla si la barra es visible
+  private scrollThreshold = 1; // Punto de scroll para fijar la barra
+  private lastScrollPosition = 0; // Última posición del scroll
+
+  private colorActual=""
+
+  private color1=false;
+  private color2=false; 
+
+  ngOnInit(): void {
+    this.lastScrollPosition = window.scrollY; // Inicializa la posición del scroll
+  }
+
+  // Escucha el evento de scroll
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    const currentScrollPosition = window.scrollY;
+
+    // Fija la barra si el scroll supera el limite
+    this.isFixed = currentScrollPosition > this.scrollThreshold;
+
+    // Oculta la barra al hacer scroll hacia abajo
+    if (currentScrollPosition > this.lastScrollPosition) {
+      this.isVisible = false;
+    } else {
+      // Muestra la barra al hacer scroll hacia arriba
+      this.isVisible = true;
+    }
+
+    this.lastScrollPosition = currentScrollPosition; // Actualiza la última posición del scroll
+
+    if (currentScrollPosition > 100 ){
+      this.color1=true
+    }
+    
 
 
-  
+
+  }
+
+  // Escucha el movimiento del mouse para mostrar la barra
+  @HostListener('window:mousemove', [])
+  onMouseMove(): void {
+    this.isVisible = true;
+  }
 
 }//termina la clase navbarComponent
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { ServiciosComponent } from './Componentes/servicios/servicios.component'
 
 import { NavbarComponent } from './Componentes/navbar/navbar.component';
 import { PreciosComponent } from './Componentes/precios/precios.component';
+import { PrincipalComponent } from './Componentes/principal/principal.component';
+import { GaleriaComponent } from './Componentes/galeria/galeria.component';
 
 
 
@@ -12,6 +14,8 @@ export const routes: Routes = [
 
   {path: 'Bienvenido', component: NavbarComponent},
   { path: 'Servicios', component: ServiciosComponent },
+  {path: 'Acerca-De', component: PrincipalComponent },
+  {path: 'Galeria', component: GaleriaComponent},
   {path: 'Precios', component: PreciosComponent},
   { path: '', redirectTo: 'Bienvenido', pathMatch: 'full' }, // Ruta por defecto
   { path: '**', redirectTo: 'Bienvenido' } // Ruta comodín para manejar errores
