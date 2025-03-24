e0565eb901fb63ce3c6736d93ffe2c4bdf5a4ea0
agustin <agustinfernandez43tb@gmail.com>
2025-03-23 22:07:29 -0300

    agregue la logica para evitar navegar a algunas rutas con el router-outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import { FooterComponent } from './Componentes/footer/footer.component';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { LayoutPrincipalComponent } from "./Componentes/layout-principal/layout-principal.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NavbarComponent,FooterComponent, RouterOutlet,CommonModule],
+  imports: [NavbarComponent, FooterComponent, RouterOutlet, CommonModule, LayoutPrincipalComponent],
      
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
@@ -20,17 +21,50 @@ export class AppComponent implements OnInit {
   title = 'peluqueriamariela';
   rutaActual:string="";
 
+  deberiaUsarRouterOutlet = true;
+  rutasExcluidas = ['/Bienvenido','/Servicios','/Acerca-De','/Galeria',];
+
+  
+
+
   constructor(private router: Router) {}
 
   ngOnInit(): void {
     initFlowbite();
 
+
+    // Detectar cambios de ruta para actualizar la lógica de visualización
     this.router.events.pipe(
       filter(event => event instanceof NavigationEnd)
     ).subscribe((event: NavigationEnd) => {
-      this.rutaActual = event.url;
+      const rutaActual = event.urlAfterRedirects;
+      
+      // Comprueba si la ruta actual está en la lista de excluidas
+      this.deberiaUsarRouterOutlet = !this.rutasExcluidas.some(ruta => 
+        rutaActual === ruta || rutaActual.startsWith(ruta + '/')
+      );
+      
+      // Si estamos en una ruta excluida, realizar scroll al elemento correspondiente
+      if (!this.deberiaUsarRouterOutlet) {
+        // Obtener el ID desde la ruta (por ejemplo, '/servicios' → 'servicios')
+        const elementId = rutaActual.split('/')[1];
+        this.scrollToElement(elementId);
+      }
     });
 
+    
+
+
+  }
+  
+
+  scrollToElement(elementId: string) {
+    setTimeout(() => {
+      const element = document.getElementById(elementId);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      }
+    }, 100); // Pequeño retraso para asegurar que los componentes estén renderizados
   }
  
   esRutaNuevoComponente() {
@@ -39,3 +73,4 @@ export class AppComponent implements OnInit {
 
 
 }
+
